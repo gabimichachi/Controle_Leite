a4d6bcaffe4bd56eb82773a67f492291639887ec
Gabrielli Michachi <gabrielli.paula@aluno.senai.br>
2025-11-28 14:58:45 -0300

    ADD: criando a conexão

---
+++ src/model/conexao.py
@@ -0,0 +1,19 @@
+import mysql.connector
+
+def criar_conexao():
+    conexao = mysql.connector.connect(
+                            host= "localhost",
+                            port=3306,
+                            user= "root",
+                            password="root",
+                            database="bd_controlleite"
+
+  ) 
+    cursor = conexao.cursor()
+
+    return conexao, cursor
+
+if __name__ == "__main__":
+    conexao, cursor = criar_conexao()
+    
+    print(conexao)
